4ed04ac9af3de453dd41ff09fdd1837c7ceb1f1c
reubenmiller <reuben.d.miller@gmail.com>
2025-07-28 00:22:54 +0200

    add debug statement

--- sys/build.rs
+++ sys/build.rs
@@ -360,19 +360,6 @@ where
     let mut cflags = vec![];
     cflags.append(&mut add_cflags);
 
-    // If --target is present in BINDGEN_EXTRA_CLANG_ARGS, add it to cflags if not already present
-    if let Ok(extra_clang_args) = std::env::var("BINDGEN_EXTRA_CLANG_ARGS") {
-        println!("cargo:warning=QUICKJSDEBUG: Trying to parse BINDGEN_EXTRA_CLANG_ARGS");
-        for arg in extra_clang_args.split_whitespace() {
-            if arg.starts_with("--target=") && !cflags.iter().any(|f| f == arg) {
-                println!("cargo:warning=QUICKJSDEBUG: Parsed --target from bindgen func BINDGEN_EXTRA_CLANG_ARGS: {}", arg.to_string());
-                cflags.push(arg.to_string());
-            }
-        }
-    } else {
-        println!("cargo:warning=QUICKJSDEBUG: Did not parse any --target value from BINDGEN_EXTRA_CLANG_ARGS (in bindgen func)");
-    }
-
     for (name, value) in defines {
         cflags.push(if let Some(value) = value {
             format!("-D{}={}", name.as_ref(), value.as_ref())
