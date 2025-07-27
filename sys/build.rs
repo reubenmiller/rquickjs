e3bc2e5bd51b72bfddeab3e962fe4ed049ac9d86
reubenmiller <reuben.d.miller@gmail.com>
2025-07-27 23:35:27 +0200

    print cargo warning

--- sys/build.rs
+++ sys/build.rs
@@ -110,12 +110,12 @@ fn main() {
         std::env::var("TARGET").unwrap().replace("-", "_")
     );
 
-    println!("rquickjs-sys/build.rs: QUICKJSDEBUG: TARGET={}", std::env::var("TARGET").unwrap_or_default());
-    println!("rquickjs-sys/build.rs: QUICKJSDEBUG: HOST={}", std::env::var("HOST").unwrap_or_default());
-    println!("rquickjs-sys/build.rs: QUICKJSDEBUG: CARGO_CFG_TARGET_OS={}", std::env::var("CARGO_CFG_TARGET_OS").unwrap_or_default());
-    println!("rquickjs-sys/build.rs: QUICKJSDEBUG: CARGO_CFG_TARGET_ENV={}", std::env::var("CARGO_CFG_TARGET_ENV").unwrap_or_default());
-    println!("rquickjs-sys/build.rs: QUICKJSDEBUG: CFLAGS={}", std::env::var("CFLAGS").unwrap_or_default());
-    println!("rquickjs-sys/build.rs: QUICKJSDEBUG: BINDGEN_EXTRA_CLANG_ARGS={}", std::env::var("BINDGEN_EXTRA_CLANG_ARGS").unwrap_or_default());
+    println!("cargo:warning=QUICKJSDEBUG: TARGET={}", std::env::var("TARGET").unwrap_or_default());
+    println!("cargo:warning=QUICKJSDEBUG: HOST={}", std::env::var("HOST").unwrap_or_default());
+    println!("cargo:warning=QUICKJSDEBUG: CARGO_CFG_TARGET_OS={}", std::env::var("CARGO_CFG_TARGET_OS").unwrap_or_default());
+    println!("cargo:warning=QUICKJSDEBUG: CARGO_CFG_TARGET_ENV={}", std::env::var("CARGO_CFG_TARGET_ENV").unwrap_or_default());
+    println!("cargo:warning=QUICKJSDEBUG: CFLAGS={}", std::env::var("CFLAGS").unwrap_or_default());
+    println!("cargo:warning=QUICKJSDEBUG: BINDGEN_EXTRA_CLANG_ARGS={}", std::env::var("BINDGEN_EXTRA_CLANG_ARGS").unwrap_or_default());
 
     println!("cargo:warning=QUICKJSDEBUG: TARGET={}", env::var("TARGET").unwrap());
     println!("cargo:warning=QUICKJSDEBUG: CARGO_CFG_TARGET_POINTER_WIDTH={}", env::var("CARGO_CFG_TARGET_POINTER_WIDTH").unwrap_or_else(|_| "<not set>".to_string()));
