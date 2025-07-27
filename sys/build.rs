33d45ad02eaa3185682a57b9d17c82d1ea636718
reubenmiller <reuben.d.miller@gmail.com>
2025-07-28 00:05:56 +0200

    add debug statement

--- sys/build.rs
+++ sys/build.rs
@@ -364,7 +364,7 @@ where
     if let Ok(extra_clang_args) = std::env::var("BINDGEN_EXTRA_CLANG_ARGS") {
         for arg in extra_clang_args.split_whitespace() {
             if arg.starts_with("--target=") && !cflags.iter().any(|f| f == arg) {
-                println!("cargo:warning=QUICKJSDEBUG: Parsed --target from BINDGEN_EXTRA_CLANG_ARGS: {}", arg.to_string());
+                println!("cargo:warning=QUICKJSDEBUG: Parsed --target from bindgen func BINDGEN_EXTRA_CLANG_ARGS: {}", arg.to_string());
                 cflags.push(arg.to_string());
             }
         }
